96bf3ab7faa622e4ac62262e2a52cccfdde84954
Jake Wharton <jw@squareup.com>
2020-12-18 10:02:09 -0500

    Switch to tabs and canonical yaml extension

--- root/app/sync.py
+++ root/app/sync.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3 -u
 
 import os
+from qbittorrentapi import Client
 
 DEBUG = os.environ['QBT_DEBUG'] == 'true'
 TAG = os.environ['QBT_TAG']
@@ -10,30 +11,29 @@ PASS = os.environ['QBT_PASS']
 
 IGNORED_TRACKER_URLS = {'** [DHT] **', '** [PeX] **', '** [LSD] **'}
 
-from qbittorrentapi import Client
 client = Client(host=HOST, username=USER, password=PASS)
 
 for torrent in client.torrents.info():
-  if DEBUG:
-    print('---', torrent.name, '---')
-    print('Tags:', torrent.tags)
+	if DEBUG:
+		print('---', torrent.name, '---')
+		print('Tags:', torrent.tags)
 
-  has_working_trackers = False
-  for tracker in torrent.trackers:
-    tracker_working = tracker.status != 4 and tracker.url not in IGNORED_TRACKER_URLS
-    if DEBUG:
-      print('Tracker:', tracker.url, tracker_working, tracker.status, tracker.msg)
-    if tracker_working:
-      has_working_trackers = True
+	has_working_trackers = False
+	for tracker in torrent.trackers:
+		tracker_working = tracker.status != 4 and tracker.url not in IGNORED_TRACKER_URLS
+		if DEBUG:
+			print('Tracker:', tracker.url, tracker_working, tracker.status, tracker.msg)
+		if tracker_working:
+			has_working_trackers = True
 
-  torrent_tags = torrent.tags.split(',')
-  if has_working_trackers:
-    if TAG in torrent_tags:
-      print('Clearing', torrent.name)
-      torrent.removeTags(TAG)
-  else:
-    if TAG not in torrent_tags:
-      print('Tagging', torrent.name)
-      torrent.addTags(TAG)
+	torrent_tags = torrent.tags.split(',')
+	if has_working_trackers:
+		if TAG in torrent_tags:
+			print('Clearing', torrent.name)
+			torrent.removeTags(TAG)
+	else:
+		if TAG not in torrent_tags:
+			print('Tagging', torrent.name)
+			torrent.addTags(TAG)
 
 print('Done')
